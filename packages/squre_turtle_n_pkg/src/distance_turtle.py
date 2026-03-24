4552b1c28385d44123c2e37c30d8b2b1a54767f3
nlulz <nirobinan@gmail.com>
2026-03-24 14:06:53 +1100

    Update distance_turtle.py

--- packages/squre_turtle_n_pkg/src/distance_turtle.py
+++ packages/squre_turtle_n_pkg/src/distance_turtle.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
 
-# Import Dependencies
 import rospy 
-from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 from turtlesim.msg import Pose
-import time 
+import math
 
 class DistanceReader:
     def __init__(self):
         
-        # Initialize the node
         rospy.init_node('turtlesim_distance_node', anonymous=True)
 
-        # Initialize subscriber: input the topic name, message type and callback signature  
-        rospy.Subscriber("/turtle1/pose", Pose,self.callback)
+        rospy.Subscriber("/turtle1/pose", Pose, self.callback)
 
-        # Initialize publisher: input the topic name, message type and msg queue size
         self.distance_publisher = rospy.Publisher('/turtle_dist', Float64, queue_size=10)
 
-        # Printing to the terminal, ROS style
-        rospy.loginfo("Initalized node!")
-        
-        # This blocking function call keeps python from exiting until node is stopped
+        self.prev_x = None
+        self.prev_y = None
+        self.total_distance = 0.0
+
+        rospy.loginfo("Initialized node!")
+
         rospy.spin()
 
-    # Whenever a message is received from the specified subscriber, this function will be called
-    def callback(self,msg):
-        rospy.loginfo("Turtle Position: %s %s", msg.x, msg.y)
+    def callback(self, msg):
 
-        ########## YOUR CODE GOES HERE ##########
-        # Calculate the distance the turtle has travelled and publish it
+        rospy.loginfo("Turtle Position: %f %f", msg.x, msg.y)
 
-        ###########################################
+        # First message → just store values
+        if self.prev_x is None:
+            self.prev_x = msg.x
+            self.prev_y = msg.y
+            return
+
+        # Calculate distance
+        dx = msg.x - self.prev_x
+        dy = msg.y - self.prev_y
+
+        distance = math.sqrt(dx*dx + dy*dy)
+        self.total_distance += distance
+
+        # Update previous position
+        self.prev_x = msg.x
+        self.prev_y = msg.y
+
+        # Publish distance
+        self.distance_publisher.publish(self.total_distance)
 
 if __name__ == '__main__': 
-
     try: 
-        distance_reader_class_instance = DistanceReader()
+        DistanceReader()
     except rospy.ROSInterruptException: 
         pass
-        
