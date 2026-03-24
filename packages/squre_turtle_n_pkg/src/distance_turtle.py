69e4f1e977bea2ad356d8f6123fd1205f9dd50e8
nlulz <nirobinan@gmail.com>
2026-03-24 13:38:41 +1100

    Add files via upload

---
+++ packages/squre_turtle_n_pkg/src/distance_turtle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Import Dependencies
+import rospy 
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+from turtlesim.msg import Pose
+import time 
+
+class DistanceReader:
+    def __init__(self):
+        
+        # Initialize the node
+        rospy.init_node('turtlesim_distance_node', anonymous=True)
+
+        # Initialize subscriber: input the topic name, message type and callback signature  
+        rospy.Subscriber("/turtle1/pose", Pose,self.callback)
+
+        # Initialize publisher: input the topic name, message type and msg queue size
+        self.distance_publisher = rospy.Publisher('/turtle_dist', Float64, queue_size=10)
+
+        # Printing to the terminal, ROS style
+        rospy.loginfo("Initalized node!")
+        
+        # This blocking function call keeps python from exiting until node is stopped
+        rospy.spin()
+
+    # Whenever a message is received from the specified subscriber, this function will be called
+    def callback(self,msg):
+        rospy.loginfo("Turtle Position: %s %s", msg.x, msg.y)
+
+        ########## YOUR CODE GOES HERE ##########
+        # Calculate the distance the turtle has travelled and publish it
+
+        ###########################################
+
+if __name__ == '__main__': 
+
+    try: 
+        distance_reader_class_instance = DistanceReader()
+    except rospy.ROSInterruptException: 
+        pass
+        
